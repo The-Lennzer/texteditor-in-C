db8f2c2cd35bba16a9da584d6981b0deee948cf1
Adithya Krishna <adithyalennzer@gmail.com>
2025-06-02 15:19:13 +0530

    enable full file read, and read multiple lines

--- kilo.c
+++ kilo.c
@@ -1,3 +1,9 @@
+
+
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include<unistd.h>
 #include<termios.h>
 #include<stdlib.h>
@@ -39,7 +45,7 @@ struct editorConfig {
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow *row;
     struct termios orig_termios;
 };
 
@@ -159,6 +165,23 @@ int getWindowSize(int *rows, int *cols){
         return 0;
     }
 }
+
+
+///////////////////
+/*** row operations */
+///////////////////
+void editorAppendRow(char *s, size_t len){
+    E.row = realloc(E.row, sizeof(erow)*(E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
+
 ///////////////////
 /*** file i/o */
 ///////////////////
@@ -170,21 +193,18 @@ void editorOpen(char *filename){
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
-    linelen = getline(&line, &linecap, fp);
-    if(linelen != -1){
+    while((linelen = getline(&line, &linecap, fp)) != -1){
         while(linelen > 0 && (line[linelen - 1] == '\n' ||
                                line[linelen - 1] == '\r'))
             linelen--;
-
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1;
+        
+        editorAppendRow(line, linelen);
         }
         free(line);
         fclose(fp);
 }
+
+
 ///////////////////
 /*** append buffer */
 ///////////////////
@@ -214,7 +234,7 @@ void editorDrawRows(struct abuf *ab){
     int y;
     for(y=0;y < E.screenrows;++y){
         if( y>=  E.numrows){
-            if(y == E.screenrows / 3){
+            if(E.numrows == 0 && E.screenrows / 3){
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome),
             "Kilo editor --version %s", KILO_VERSION);
@@ -230,9 +250,9 @@ void editorDrawRows(struct abuf *ab){
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row.size;
+            int len = E.row[y].size;
             if (len>E.screencols) len = E.screencols;
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, E.row[y].chars, len);
         }
         abAppend(ab, "\x1b[K", 3);
 
@@ -326,6 +346,7 @@ void initEditor(){
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.row = NULL;
     
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
