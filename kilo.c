0ed81f4cf6dd3c87e45d3d87f5ab12759d25ddf8
Adithya Krishna <adithyalennzer@gmail.com>
2025-06-02 17:04:57 +0530

    addded scrollability

--- kilo.c
+++ kilo.c
@@ -42,6 +42,7 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rowoff;
     int screenrows;
     int screencols;
     int numrows;
@@ -230,10 +231,20 @@ void abFree(struct abuf *ab){
 ///////////////////
 /*** output */
 ///////////////////
+void editorScroll(){
+    if (E.cy < E.rowoff){
+        E.rowoff = E.cy;
+    }
+    if(E.cy >= E.rowoff + E.screenrows){
+        E.rowoff = E.cy - E.screenrows + 1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab){
     int y;
     for(y=0;y < E.screenrows;++y){
-        if( y>=  E.numrows){
+        int filerow = y + E.rowoff;
+        if( filerow >=  E.numrows){
             if(E.numrows == 0 && E.screenrows / 3){
                 char welcome[80];
                 int welcomelen = snprintf(welcome, sizeof(welcome),
@@ -250,9 +261,9 @@ void editorDrawRows(struct abuf *ab){
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row[y].size;
+            int len = E.row[filerow].size;
             if (len>E.screencols) len = E.screencols;
-            abAppend(ab, E.row[y].chars, len);
+            abAppend(ab, E.row[filerow].chars, len);
         }
         abAppend(ab, "\x1b[K", 3);
 
@@ -263,6 +274,7 @@ void editorDrawRows(struct abuf *ab){
 }
 
 void editorRefreshScreen(){
+    editorScroll();
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
@@ -296,7 +308,7 @@ void editorMoveCursor(int key){
         if(E.cy != 0) E.cy--;
         break;
     case ARROW_DOWN:
-        if(E.cy != E.screenrows-1) E.cy++;
+        if(E.cy < E.numrows) E.cy++;
         break;
     }
 }
@@ -345,6 +357,7 @@ void editorProcessKeypress(){
 void initEditor(){
     E.cx = 0;
     E.cy = 0;
+    E.rowoff = 0;
     E.numrows = 0;
     E.row = NULL;
     
